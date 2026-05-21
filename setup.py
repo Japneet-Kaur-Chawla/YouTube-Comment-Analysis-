2f1315f6d6b25d0e45f0866e9329cfcb2505d84f
Japneet-KaurChawla <japneetkaurchawla231106@gmail.com>
2026-05-21 21:53:21 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project where we can know the description about the comments especially designed for YouTube.',
+    author='Japneet Kaur Chawla',
+    license='',
+)
